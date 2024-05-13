e151f1ec8f3f4c9f6d294e938beef466b2051f3f
Isabela Migliorini <isabelamigli@gmail.com>
2024-05-13 15:39:13 -0300

    entrada e saída de dados

--- src/contaBancaria/ContaTerminal.java
+++ src/contaBancaria/ContaTerminal.java
@@ -1,10 +1,39 @@
 package contaBancaria;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
+		// TODO: conhecer e importar a classe scanner
+		// exibir as mensagens para o usuário
+		// obter pelo scanner os valores digitados no terminal
+		// exibir a mensagem de conta criada
 		
-
+		String numeroConta;
+		int agencia;
+		String nomeCliente;
+		double saldo;
+		Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+		
+		System.out.println("Por favor, digite o seu nome:");
+		nomeCliente = scanner.next();
+		
+		System.out.println("Por favor, digite o número da agência:");
+		agencia = scanner.nextInt();
+		
+		System.out.println("Por favor, digite o número da conta:");
+		numeroConta = scanner.next();
+		
+		System.out.println("Por favor, digite o saldo para saque:");
+		saldo = scanner.nextDouble();
+		
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+				+ "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo +
+				" já está disponível para saque!");
+		
+		
+		scanner.close();
 	}
-
+	
 }
